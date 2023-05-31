010b0d6683c54cbc4aeb30ed83247ad65a96c30b
saddy <maliksadaf99@gmail.com>
2023-05-31 18:47:59 +0530

    authenticated user before opening page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_list, only: %i[ show edit update destroy ]
-
-
+  before_action :authenticate_user!
 
   # GET /lists or /lists.json
    def index
